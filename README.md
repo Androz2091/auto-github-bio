44d6e13fdba27e1a1817b91bf059c251afdc9b61
Androz2091 <androz2091@gmail.com>
2019-12-13 14:02:24 +0100

    :tada: Init repository

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# auto-github-bio
-📚Automatized Github biography using openweathermap.org API
+# Automatized Github Biography
+
+## Installation
+
+### [Github] Create your personal access token
+
+You have to create a **personal access token**, to allow your app to update your biography. Click [here](https://github.com/settings/tokens/new) to go to the personal access token creation page. Select the `User` field (Update all user data) and click `Generate new token`. Copy and paste it in your **config.json** file (`github` field).
+
+### [OpenWeatherMap] Get your application key
+
+You have to get your **openweathermap.org api key**, to allow your app to get the weather of your favourite city. Register [here](https://openweathermap.org/home/sign_up), then go on [your dashboard](https://home.openweathermap.org/api_keys) to get your key. Copy and paste it in your **config.json** file (`weather` field).
+
+### [Crontab] Run the script every 5 minutes
+
+You have to edit the **crontab table** using `crontab -e`. Then, add the following line to this file:  
+```sh
+5 * * * * cd /path/to/auto-github-bio && /usr/bin/python3 /path/to/auto-github-bio/main.py >> ~/cron.log 2>&1
+```
+This will run the script and update your biography every 5 minutes.
+
+### That's it
+
+Congratulations, you have successfully installed Automatized Github Biography. Feel free to open an issue if necessary!
+
+### Support me on patreon
+
+Feel free to support me on Patreon so that I can continue to finance my open source projects! Thanks!  
+
+<a href="https://www.patreon.com/bePatron?u=20304709"><img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png"></a>
---
+++ main.py
@@ -0,0 +1,87 @@
+# Used to make call to the github API
+import requests
+# Used to read the config file
+import json
+# Used to display the last update
+from datetime import datetime
+
+# Read the config file
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+
+def update_bio(text):
+    """
+    Update the Github biography of the user
+    
+    Args:
+        text (str): The new biography content
+
+    Returns:
+        data (any): The Github response
+    """
+    url         = "https://api.github.com/user"
+    headers     = { "Authorization": "token "+config["github"], "Content-Type" : "text/plain" }
+    response    = requests.patch(url, '{ "bio": "'+text+'" }', headers=headers)
+    data        = response.json()
+    return data
+
+
+def get_weather_of(city):
+    """
+    Get the weather of a specific city
+    
+    Args:
+        city (str): The name of the city whose weather we want
+
+    Returns:
+        weather (str): The weather of the city
+    """
+    # Open Weather Map API Base url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+config["weather"]+"&q="+city
+    response = requests.get(base_url)
+    data     = response.json()
+    # Returns weather info (snow, rain, clouds, etc...)
+    return data["weather"][0]["main"]
+
+
+def generate_bio_content(weather):
+    """
+    Generate the bio content
+
+    Args:
+        weather (str): The weather of the city
+
+    Returns:
+        bio_content (str): The final bio content
+    """
+    # The current time (hours and minutes)
+    now = datetime.now().strftime("%H:%M")
+    # Returns the final string wich contain the city, the weather, the last update and the credits
+    return "Current "+config["city"]+" weather: "+weather.upper()+" | Last update: "+now+" | Made by Androz2091 using Python"
+
+
+if(__name__ == "__main__"):
+    # Get the weather of the city
+    weather = get_weather_of(config["city"])
+    # Get the bio content
+    bio_content = generate_bio_content(weather)
+    # Update the biography
+    status = update_bio(bio_content)
+
+    # Log
+    log_prefix = "["+datetime.now().strftime("%H:%M")+"]"
+    # If there is a login attr in the status object, it means the operation is successfull
+    if "login" in status:
+        print(log_prefix+" Successfully updated biography")
+    else:
+        # If Github returned a message
+        if "message" in status:
+            # If the error is caused by the personal access token
+            if status["message"] == "Bad credentials":
+                print(log_prefix+" Seems like your Github personal access token is invalid...")
+            else:
+                print(log_prefix+" Something happened. Message is the following: "+status["message"])
+        # If Github didn't return anything
+        else:
+            print(log_prefix+" Something happened. Here is the Github response: "+status["message"])
